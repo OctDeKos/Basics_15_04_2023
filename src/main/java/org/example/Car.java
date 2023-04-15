841def19a6a3c696a7e73cf6632438d759c6d3fc
OctDeKos <ymdyadin@gmail.com>
2023-04-15 13:51:44 +0300

    First

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Car {
+    public void build() {
+        System.out.println("You must build a new car");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car(); //1 - объект этого класса. 2 - любое имя которое мы даем. 3 - создаем сам объект на основании чертежа
+        car.build(); //вызываем метод по новому имени
+
+    }
+}
